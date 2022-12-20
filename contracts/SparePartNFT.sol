8334a194876685a8fd251046dd3878bf33287e2e
Senay <senaykarisful@gmail.com>
2022-12-20 18:58:18 +0400

    minor fix

--- contracts/SparePartNFT.sol
+++ contracts/SparePartNFT.sol
@@ -7,6 +7,10 @@ import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
+interface IDeviceNFT {
+
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4);
+}
 contract SparePartNFT is ERC721, ERC721URIStorage {
 
 
@@ -18,7 +22,14 @@ contract SparePartNFT is ERC721, ERC721URIStorage {
         return tokenCount_;
     }
 
-    function safeTransfer () {}
+    function safeTransfer (address _from, address _to, uint256 _tokenId, bytes _data) external{
+        transferFrom(_from, _to, _tokenId);
+        if (isContract(_to)) {
+            bytes4 val = IDeviceNFT(_to).onERC721Received(msg.sender, _from, _tokenId, _data);
+            // require(retval == ERC721_RECEIVED_OLD);
+        }
+
+    }
 
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
@@ -32,5 +43,13 @@ contract SparePartNFT is ERC721, ERC721URIStorage {
     {
         return super.tokenURI(tokenId);
     }
+
+    function isContract(address _addr) internal view returns (bool) {
+        uint256 size;
+        assembly {size := extcodesize(_addr)}
+        return size > 0;
+    }
+
+
 
 }
