cc5c408d48dd661fc22606e85f229706f88bfdaf
Senay <senaykarisful@gmail.com>
2022-12-30 23:43:15 +0400

    minor fix

--- contracts/SparePartNFT.sol
+++ contracts/SparePartNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
@@ -9,24 +9,44 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 interface IDeviceNFT {
 
-    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4);
+    // function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4);
+    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes memory _data) external returns(bytes4);
 }
 contract SparePartNFT is ERC721, ERC721URIStorage {
 
+    // string metadata = "https://ipfs.io/ipfs/Qme7ss3ARVqxv6rXVPiikMJ8u2NLqmgszg13pYrDKEoiu"
+    constructor() ERC721("SPToken", "SPT") {}
 
-    function mint(address _to) public returns (uint256) {
+    event ReplacementPartNFTMinted(uint256 indexed _tokenId, address _owner, string _tokenURI);
+    event Retval (bytes4 retval);
+    event TokenTransferedToParentNFT (uint256 _childTokenId, address _parentContract, bytes _encodedData);
+
+    uint256 public tokenCount;
+
+    // tokenId => token owner
+    mapping(uint256 => address) internal tokenIdToOwner;
+    // token owner address => token count
+    mapping(address => uint256) internal OwnerToTokenCount;
+
+
+    function mint(address _to, string calldata _metadata) public returns (uint256) {
         tokenCount++;
-        uint256 tokenCount_ = tokenCount;
-        tokenIdToOwner[tokenCount_] = _to;
+        uint256 tokenId = tokenCount;
+        tokenIdToOwner[tokenId] = _to;
         OwnerToTokenCount[_to]++;
-        return tokenCount_;
+        _safeMint(_to, tokenId);
+        _setTokenURI(tokenId, _metadata);
+        emit ReplacementPartNFTMinted (tokenId, _to, _metadata);
+        return tokenId;
     }
 
-    function safeTransfer (address _from, address _to, uint256 _tokenId, bytes _data) external{
+    function safeTransfer (address _from, address _to, uint256 _tokenId, bytes memory _data) external{
         transferFrom(_from, _to, _tokenId);
         if (isContract(_to)) {
-            bytes4 val = IDeviceNFT(_to).onERC721Received(msg.sender, _from, _tokenId, _data);
+            bytes4 val = IDeviceNFT(_to).onERC721Received(msg.sender, _from,_tokenId, _data);
             // require(retval == ERC721_RECEIVED_OLD);
+            emit Retval (val);
+            emit TokenTransferedToParentNFT (_tokenId, _to, _data);
         }
 
     }
@@ -49,6 +69,12 @@ contract SparePartNFT is ERC721, ERC721URIStorage {
         assembly {size := extcodesize(_addr)}
         return size > 0;
     }
+
+    function toBytes(uint256 x) public view returns (bytes memory b) {
+        b = new bytes(32);
+        assembly { mstore(add(b, 32), x) 
+    }
+}
 
 
 
